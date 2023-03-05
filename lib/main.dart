6a7e316d73258fb778827f9051d955b09da26d05
Anastasya Dian Irawati <anastasyadn@MacBook-Air-Anastasya.local>
2023-03-05 20:30:39 +0700

    Tugas 1 Praktikum TPM

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'page_login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tugas 1 Praktikum TPM',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
